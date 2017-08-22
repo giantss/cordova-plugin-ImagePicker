7a53e623ef2babd086b5af8f9a910ab82022a896
阮承志 <ruancz@chenghideMacBook-Pro.local>
2017-08-22 14:27:48 +0800

    将原图按钮隐藏

--- src/ios/ImagePickerMain.m
+++ src/ios/ImagePickerMain.m
@@ -29,7 +29,7 @@
     _enablePickingVideo = false;
     _enablePickingImage = true;
     _enablePickingGif = false;
-    _enablePickingOriginalPhoto = true;
+    _enablePickingOriginalPhoto = false;
     _enableShowSheet = false;
     _enableCrop = false;
     _enableCircleCrop = false;
